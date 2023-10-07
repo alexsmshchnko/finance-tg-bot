a766f002d19daa2814d4886a0f283f8d85f58e34
alexsmshchnko <quilematiss@gmail.com>
2023-10-07 21:27:39 +0300

    core: init project

---
+++ finance_bot.go
@@ -0,0 +1,14 @@
+package main
+
+import "log"
+
+func run() error {
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal()
+	}
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module finance-tg-bot
+
+go 1.21.1
