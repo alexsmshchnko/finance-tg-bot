17a278bbcced95cffe6ae95367e6d8e6f4251c1b
alexsmshchnko <quilematiss@gmail.com>
2023-10-07 21:44:35 +0300

    core: add tgbotapi

--- finance_bot.go
+++ finance_bot.go
@@ -1,8 +1,32 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+var (
+	gToken string
+	gBot   *tgbotapi.BotAPI
+)
+
+func init() {
+	if gToken = os.Getenv("BOT_TOKEN"); gToken == "" {
+		panic(fmt.Errorf("failed to load env variable %s", "BOT_TOKEN"))
+	}
+
+	var err error
+	if gBot, err = tgbotapi.NewBotAPI(gToken); err != nil {
+		log.Panic(err)
+	}
+}
 
 func run() error {
+	//fmt.Println(gBot)
+
 	return nil
 }
 
